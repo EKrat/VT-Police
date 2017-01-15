ed18ffde6239f6b5433b6b74103b7c4a0252ceed
Eric Kratochvil <Eric_Kratochvil@hks17.harvard.edu>
2017-01-14 19:32:29 -0500

    changed from bright yellow for White to dark yellow so its easier on the eyes- less bright.

--- draw.js
+++ draw.js
@@ -15,7 +15,7 @@ var y = d3.scaleRadial()
     .range([innerRadius, outerRadius]);
 
 var z = d3.scaleOrdinal()
-    .range(["#B9006E", "#CCC51C", "#F05A28", "#2A0256", "#a05d56", "#FFE600", "#ff8c00"]);
+    .range(["#B9006E", "#FFE600", "#F05A28", "#2A0256", "#a05d56", "#CCC51C", "#ff8c00"]);
 
 d3.csv("eric_data.csv", function(d, i, columns) {
   for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
